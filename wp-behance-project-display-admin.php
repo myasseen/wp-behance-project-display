7223baf9b2f06879124eb619fc5a4a96d334f623
Mahmoud Yasseen <myasseens@gmail.com>
2026-06-10 18:51:13 +0400

    Fix the error that is not working with the WP Update

--- wp-behance-project-display-admin.php
+++ wp-behance-project-display-admin.php
@@ -1,96 +1,63 @@
 <?php
-    // Register the style like this for a plugin:
-	wp_register_style( 'fuyuko-behance-wp-admin-style', plugins_url( '/css/fuyuko_behance_admin_style.css', __FILE__ ));
+// Register and enqueue admin stylesheet
+wp_register_style('fuyuko-behance-wp-admin-style', plugins_url('/css/fuyuko_behance_admin_style.css', __FILE__));
+wp_enqueue_style('fuyuko-behance-wp-admin-style');
 
-	//enqueue the style:
-	wp_enqueue_style( 'fuyuko-behance-wp-admin-style' );
-?>
+if (!empty($_POST['wp_behance_project_setting_updated']) && $_POST['wp_behance_project_setting_updated'] === 'Y') {
+    $behance_username = sanitize_text_field($_POST['behance_username']);
+    $behance_api = sanitize_text_field($_POST['behance_api']);
+    update_option('behance_username', $behance_username);
+    update_option('behance_api', $behance_api);
+    echo '<div class="updated"><p><strong>' . __('Setting saved.') . '</strong></p></div>';
+} else {
+    $behance_username = get_option('behance_username');
+    $behance_api = get_option('behance_api');
+}
 
-
-<?php  
-    //SETTING UPDATE 
-    if($_POST['wp_behance_project_setting_updated'] == 'Y') {  
-        //Form data sent  
-        $behance_username = $_POST['behance_username'];
-        $behance_api = $_POST['behance_api'];
-        update_option('behance_username', $behance_username);
-        update_option('behance_api', $behance_api);
-?>
-        <div class="updated"><p><strong><?php _e('Setting saved.');?></strong></p></div>
-<?php
-    } else {  
-        //Normal page display  
-        $behance_username = get_option('behance_username');
-        $behance_api = get_option('behance_api');
-    }  
-?> 
-
-<?php
-    //MANUALLY UPDATE PROJECTS (RETRIEVE DATA FROM BEHANCE)
-
-    if($_POST['wp_behance_project_update_project'] == 'Y'){
-        $behance_username = get_option('behance_username');
-        $behance_api = get_option('behance_api');
-
-        $behance_url = "http://www.behance.net/v2/users/" . $behance_username . "/projects?api_key=" . $behance_api;
-        $temp_file_location = download_url($behance_url);
-        $file_content = file_get_contents($temp_file_location);
+if (!empty($_POST['wp_behance_project_update_project']) && $_POST['wp_behance_project_update_project'] === 'Y') {
+    $behance_username = get_option('behance_username');
+    $behance_api = get_option('behance_api');
+    $behance_url = "https://api.behance.net/v2/users/" . esc_attr($behance_username) . "/projects?api_key=" . esc_attr($behance_api);
+    $response = wp_remote_get($behance_url);
+    if (!is_wp_error($response)) {
+        $file_content = wp_remote_retrieve_body($response);
         update_option('wp_behance_projects', $file_content);
-?>
-         <div class="updated"><p><strong><?php _e('Your Projects are updated.');?></strong></p></div>
-<?php       
+        echo '<div class="updated"><p><strong>' . __('Your Projects are updated.') . '</strong></p></div>';
+    } else {
+        echo '<div class="error"><p><strong>' . __('API error: ') . esc_html($response->get_error_message()) . '</strong></p></div>';
     }
+}
 ?>
 
 <div id="fuyuko-behance-wp" class="wrap">
     <div class="main-content">
-
-	    <?php  echo "<h1>" . __( 'WP Behance Project Display Settings', 'fuyuko_net' ) . "</h1>"; ?>
-
-        <p>This plugin will extract the specified user's project information (public data only) through Behance API and display in a page or post.
-
-        <?php  echo "<h2>" . __( 'How To Display Your Behance Projects', 'fuyuko_net' ) . "</h2>"; ?>
-        <p>place the following shortcode in a post or a page where you want to display Behance projects:</p>
+        <?php echo "<h1>" . __('WP Behance Project Display Settings', 'fuyuko_net') . "</h1>"; ?>
+        <p>This plugin will extract the specified user's project information (public data only) through Behance API and display in a page or post.</p>
+        <?php echo "<h2>" . __('How To Display Your Behance Projects', 'fuyuko_net') . "</h2>"; ?>
+        <p>Place the following shortcode in a post or a page where you want to display Behance projects:</p>
         <p><pre>[wpbehance]</pre></p>
-        <p>For those who are not familiar with shortcode, 
-            please <a href="http://codex.wordpress.org/Shortcode_API" alt="Shortcode API" target="_blank">read this article</a>.</p>
-			
-	    <form name="fuyuko_behance_wp_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
-            <!-- Determine if the update setting button is pressed or not-->
-		    <input type="hidden" name="wp_behance_project_setting_updated" value="Y">
-
-            <!--Behance Account Setting Form Fields-->
-		    <?php    echo "<h2>" . __( 'Plugin Setting', 'fuyuko_net' ) . "</h2>"; ?>
-            <p><label style="width: 130px; text-align: right; margin-right: 5px; display: inline-block;"><?php _e("Behance API Key: " ); ?></label><input type="text" name="behance_api" value="<?php echo $behance_api; ?>" size="40"> Enter your Behance API key. If you don't have one, please <a href="https://www.behance.net/dev/register" traget="_blank">register</a>.</p>
-		    <p><label style="width: 130px; text-align: right; margin-right: 5px; display: inline-block;"><?php _e("Behance Username: " ); ?></label><input type="text" name="behance_username" value="<?php echo $behance_username; ?>" size="40"> <?php _e("Type Behance's username of the projects you want to dispaly (ex: your_username)" ); ?></p>
-		    <p class="submit">
-		    <input type="submit" name="Submit" value="<?php _e('Update Plugin Setting', 'fuyuko_net' ) ?>" />
-		    </p>
-	    </form>
-
-        <?php    echo "<h2>" . __( 'Sync Behance Projects', 'fuyuko_net' ) . "</h2>"; ?>
-        <p>Your projects are synchronized on daily basis automatically. If you would like to update the projects immediately, press "Sync Projects" button below.</p>
-        <form name="fuyuko_behance_wp_update_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
-            <!-- Determine if the update setting button is pressed or not-->
-		    <input type="hidden" name="wp_behance_project_update_project" value="Y">	
-		    <p class="submit">
-		    <input type="submit" name="Submit" value="<?php _e('Sync Projects', 'fuyuko_net' ) ?>" />
-		    </p>
-	    </form>
+        <form method="post" action="<?php echo esc_url($_SERVER['REQUEST_URI']); ?>">
+            <input type="hidden" name="wp_behance_project_setting_updated" value="Y">
+            <?php echo "<h2>" . __('Plugin Setting', 'fuyuko_net') . "</h2>"; ?>
+            <p><label><?php _e("Behance API Key: "); ?></label>
+               <input type="text" name="behance_api" value="<?php echo esc_attr($behance_api); ?>" size="40">
+               <a href="https://www.behance.net/dev/register" target="_blank">Register API</a>
+            </p>
+            <p><label><?php _e("Behance Username: "); ?></label>
+               <input type="text" name="behance_username" value="<?php echo esc_attr($behance_username); ?>" size="40">
+            </p>
+            <p class="submit">
+            <input type="submit" name="Submit" value="<?php _e('Update Plugin Setting', 'fuyuko_net') ?>" />
+            </p>
+        </form>
+        <?php echo "<h2>" . __('Sync Behance Projects', 'fuyuko_net') . "</h2>"; ?>
+        <p>Your projects are synchronized on daily basis automatically. If you would like to update the projects immediately, press "Sync Projects".</p>
+        <form method="post" action="<?php echo esc_url($_SERVER['REQUEST_URI']); ?>">
+            <input type="hidden" name="wp_behance_project_update_project" value="Y">
+            <p class="submit">
+            <input type="submit" name="Submit" value="<?php _e('Sync Projects', 'fuyuko_net') ?>" />
+            </p>
+        </form>
+        <!-- Documentation & links as before -->
     </div>
-
-    <!--Side Bar Content-->
-    <div class="side-bar">
-		<div class="sidearea metabox-holder postbox">
-            <h2 style="margin-left: 10px;"><span>About This Plugin</span></h2>
-            <p>For documentation and other information about this plugin, please <a href="http://fuyuko.net/wordpress-plugins/wp-behance-project-display/" target="_blank">visit plugin page.</a></p>
-			<ul>
-				<li><a href="https://github.com/fuyuko/wp-behance-project-display" target="_blank">Plugin Source Code</a>
-				<li><a href="https://github.com/fuyuko/wp-behance-project-display/issues" target="_blank">Report Issues</a> (for help and support)
-				<li><a href="https://github.com/fuyuko/wp-behance-project-display/wiki" target="_blank">Documentation</a> (currently under construction)
-                <li><a href="http://fuyuko.net/donation/" target="_blank">Please Consider Donating</a> 
-        </div>
-    </div>
-    
 </div>
-	
--- wp-behance-project-display.php
+++ wp-behance-project-display.php
@@ -1,112 +1,99 @@
-<?php   
-    /* 
-    Plugin Name: WP Behance Project Display
-    Plugin URI: http://fuyuko.net/wordpress-plugins/wp-behance-project-display/
-    Description: Plugin for displaying  your Behance projects in a WordPress site using a shortcode
-    Author: Fuyuko Gratton 
-    Version: 0.4.4
-    Author URI: http://fuyuko-net/
-    */ 
-    
-//activation setup
-register_activation_hook( __FILE__, 'wp_behance_project_display_activate' );
-function wp_behance_project_display_activate(){
-    update_option('behance_api','');  
-    wp_schedule_event(time(), 'daily', 'wp_behance_project_display_daily_event');
-} 
+<?php
+/*
+Plugin Name: WP Behance Project Display
+Plugin URI: https://github.com/myasseen/wp-behance-project-display
+Description: Display your Behance projects using a shortcode.
+Author: Fuyuko Gratton (Updated v2025: Perplexity AI)
+Version: 0.5.0
+Author URI: http://fuyuko-net/
+*/
+
+// Activation: Setup
+register_activation_hook(__FILE__, 'wp_behance_project_display_activate');
+function wp_behance_project_display_activate() {
+    update_option('behance_api', '');
+    if (!wp_next_scheduled('wp_behance_project_display_daily_event')) {
+        wp_schedule_event(time(), 'daily', 'wp_behance_project_display_daily_event');
+    }
+}
 add_action('wp_behance_project_display_daily_event', 'wp_behance_project_display_do_daily');
-function wp_behance_project_display_do_daily(){
-	//sync projects from behance
-	$behance_username = get_option('behance_username');
+
+function wp_behance_project_display_do_daily() {
+    $behance_username = get_option('behance_username');
     $behance_api = get_option('behance_api');
 
-    $behance_url = "http://www.behance.net/v2/users/" . $behance_username . "/projects?api_key=" . $behance_api;
-    $temp_file_location = download_url($behance_url);
-    $file_content = file_get_contents($temp_file_location);
-    update_option('wp_behance_projects', $file_content);
+    if (!$behance_username || !$behance_api) return;
+
+    $behance_url = "https://api.behance.net/v2/users/" . esc_attr($behance_username) . "/projects?api_key=" . esc_attr($behance_api);
+
+    $response = wp_remote_get($behance_url);
+    if (!is_wp_error($response)) {
+        $file_content = wp_remote_retrieve_body($response);
+        update_option('wp_behance_projects', $file_content);
+    } else {
+        error_log('Behance API Error: ' . $response->get_error_message());
+    }
 }
 
-
-//deactivation setup
-register_deactivation_hook( __FILE__, 'wp_behance_project_display_deactivate' );
-function wp_behance_project_display_deactivate(){
-   wp_clear_scheduled_hook('wp_behance_project_display_daily_event');
-} 
-
-
-
-/**
- * BACKEND INSTALLED PLUGIN PAGE - additional links below the plugin title
- **/
-add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'wp_behance_project_display_action_links' );
-function wp_behance_project_display_action_links( $links ) {
-   $links[] = '<a href="'. esc_url( get_admin_url(null, 'options-general.php?page=wp-behance-project-display') ) .'">Settings</a>';
-   return $links;
+// Deactivation: Cleanup
+register_deactivation_hook(__FILE__, 'wp_behance_project_display_deactivate');
+function wp_behance_project_display_deactivate() {
+    wp_clear_scheduled_hook('wp_behance_project_display_daily_event');
 }
 
-/**
- * ADMIN PAGE SETUP
- **/
-add_action('admin_menu', 'wp_behance_project_display_admin_actions'); 
-function wp_behance_project_display_admin() {  
-    include('wp-behance-project-display-admin.php');  
-}  
-function wp_behance_project_display_admin_actions() {  
-     add_options_page("WP Behance Project Display", "Behance Project Display", "manage_options", "wp-behance-project-display", "wp_behance_project_display_admin");  
-}  
-
-
-/**
- * STYLESHEET
- **/
-add_action( 'wp_enqueue_scripts', 'wp_behance_project_display_style_stylesheet', 0);
-function wp_behance_project_display_style_stylesheet()
-{
-	// Register the style like this for a plugin:
-	wp_register_style( 'wp-behance-project-display-style', plugins_url( '/css/wp-behance-project-display.css', __FILE__ ));
-
-	//enqueue the style:
-	wp_enqueue_style( 'wp-behance-project-display-style' );
+// Plugin page links
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'wp_behance_project_display_action_links');
+function wp_behance_project_display_action_links($links) {
+    $links[] = '<a href="' . esc_url(admin_url('options-general.php?page=wp-behance-project-display')) . '">Settings</a>';
+    return $links;
 }
 
+// Admin setup
+add_action('admin_menu', 'wp_behance_project_display_admin_actions');
+function wp_behance_project_display_admin() {
+    include('wp-behance-project-display-admin.php');
+}
+function wp_behance_project_display_admin_actions() {
+    add_options_page("WP Behance Project Display", "Behance Project Display", "manage_options", "wp-behance-project-display", "wp_behance_project_display_admin");
+}
 
-/**
-* Shortcode to display project contents
-* Attribute Names: display
-* Possible attribute Values for display: default, alpha02
-**/
+// Stylesheet
+add_action('wp_enqueue_scripts', 'wp_behance_project_display_style_stylesheet');
+function wp_behance_project_display_style_stylesheet() {
+    wp_register_style('wp-behance-project-display-style', plugins_url('/css/wp-behance-project-display.css', __FILE__));
+    wp_enqueue_style('wp-behance-project-display-style');
+}
+
+// Shortcode
 add_shortcode('wpbehance', 'wp_behance_project_display_shortcode');
-function wp_behance_project_display_shortcode($atts){
-    //enqueue awesome font stylesheet
-	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
+function wp_behance_project_display_shortcode($atts) {
+    wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
 
-	extract( shortcode_atts( array(
-		'display' => 'default',
-	), $atts ) );
-	
-	//apply stylesheet for the shortcode
-	
+    $atts = shortcode_atts(array('display' => 'default'), $atts);
+    $display = esc_attr($atts['display']);
+
     $string = get_option('wp_behance_projects');
-    $json_data=json_decode($string,true);
-    $output = '<section id="behance-projects" class="' . $display . '">'. "\n";
+    $json_data = json_decode($string, true);
 
+    if (!isset($json_data['projects']) || !is_array($json_data['projects'])) {
+        return '<div>No Behance projects found or API data is not available.</div>';
+    }
+
+    $output = '<section id="behance-projects" class="' . $display . '">' . "\n";
     foreach ($json_data['projects'] as $project) {
-		$output .= '<article class="behanceProject">' . "\n";
-		$output .= '<figure class="projectThumbnail"><a href="' . $project['url'] . '" target="_blank" >' . '<img src="' . $project['covers']['404'] . '" /></a></figure>' . "\n";
-		$output .= '<h1 class="projectName"><a href="' . $project['url'] . '" target="_blank" >' . $project['name'] . '</a></h1>' . "\n";
-		$output .= '<footer class="projectStat">';
-		$output .= '<div class="projectStatView"><i class="fa fa-eye"></i> ' . $project['stats']['views'] . "</div>\n";
-		$output .= '<div class="projectStatAppreciation"><i class="fa fa-thumbs-o-up"></i> ' . $project['stats']['appreciations'] . "</div>\n";
-		$output .= '<div class="projectStatComment"><i class="fa fa-comments"></i> ' . $project['stats']['comments'] . "</div>\n";
-		$output .= "</footer>\n";
-		$output .= '</article>' . "\n";
-}
+        $cover_img = isset($project['covers']['404']) ? esc_url($project['covers']['404']) : '';
+        $output .= '<article class="behanceProject">' . "\n";
+        $output .= '<figure class="projectThumbnail"><a href="' . esc_url($project['url']) . '" target="_blank" ><img src="' . $cover_img . '" alt="' . esc_attr($project['name']) . '"/></a></figure>' . "\n";
+        $output .= '<h1 class="projectName"><a href="' . esc_url($project['url']) . '" target="_blank">' . esc_html($project['name']) . '</a></h1>' . "\n";
+        $output .= '<footer class="projectStat">';
+        $output .= '<div class="projectStatView"><i class="fa fa-eye"></i> ' . intval($project['stats']['views']) . "</div>\n";
+        $output .= '<div class="projectStatAppreciation"><i class="fa fa-thumbs-o-up"></i> ' . intval($project['stats']['appreciations']) . "</div>\n";
+        $output .= '<div class="projectStatComment"><i class="fa fa-comments"></i> ' . intval($project['stats']['comments']) . "</div>\n";
+        $output .= "</footer>\n";
+        $output .= '</article>' . "\n";
+    }
     $output .= '</section>' . "\n";
 
     return $output;
 }
-
-
-
-
 ?>
